91d3458376dec73aad39d13ee4771590e226cc54
Santosh-Kumar-Sah <sureshsirf886@gmail.com>
2026-03-26 14:35:48 +0530

    added api running for test

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
